69bffb99e13559a21f824183fdaf012c2ee9377f
joaquinlepeS <joa.lepe@duocuc.cl>
2025-09-29 13:49:59 -0300

    Creacion de prueba del HomeScreen actividad 2.1.7

---
+++ app/src/main/java/com/example/app_kotlin/ui/screens/HomeScreen.kt
@@ -0,0 +1,58 @@
+package com.example.app_kotlin.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import  com.example.app_kotlin.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(){
+    Scaffold (
+        topBar = {
+            TopAppBar(title = { Text("Mi app kotlin") })
+        }
+    ){innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Text(text = "Bienvenido")
+            Button(onClick = {/*accion futura*/}) {
+                Text("Presioname")
+            }
+            Image(
+                painter = painterResource( R.drawable.ic_launcher_background),
+                contentDescription = "Logo App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun homeScreenPreview(){
+    HomeScreen()
+}
